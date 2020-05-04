2aee7ab6852add5ee6e0751047c010ed06f08896
Gaurav Gawande <gaurav.gawande@yahoo.com>
2020-05-04 22:35:51 +0530

    Seed  Data

---
+++ EmployeeManagement.DataAccess/ModelBuilderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using EmployeeManagement.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.DataAccess
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder builder)
+        {
+            builder.Entity<Employee>().HasData(
+                new Employee() { Id = 1, Name = "Ram", Department = Department.IT, Email = "Ram@Primes.com" },
+                new Employee() { Id = 2, Name = "Sham", Department = Department.IT, Email = "Sham@Primes.com" }
+            );
+        }
+    }
+}
